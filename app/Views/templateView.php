73ceb61273bbe0b90f7218d79bec3c767d037c3e
languder2 <163087417+languder2@users.noreply.github.com>
2024-04-11 16:30:57 +0300

    change: work end - bootstrap
    Signed-off-by: languder2 <163087417+languder2@users.noreply.github.com>

--- app/Views/templateView.php
+++ app/Views/templateView.php
@@ -1,21 +1,48 @@
 <!DOCTYPE html>
-<html>
+<html lang="ru">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?=$title??""?></title>
     <script defer src="<?= base_url('js/public/main.js');?>"></script>
+    <link href="<?=base_url('css/lib/bootstrap.min.css')?>" rel="stylesheet" type="text/css">
+    <link href="https://mgu-mlt.ru/wp-content/themes/shabloner_232/css/font-icons.css" rel="stylesheet" type="text/css">
+    <link href="<?=base_url('css/public/media.css')?>" rel="stylesheet" type="text/css">
     <link href="<?=base_url('css/public/vars.css')?>" rel="stylesheet" type="text/css">
     <link href="<?=base_url('css/public/base.css')?>" rel="stylesheet" type="text/css">
 </head>
 <body>
-<div class="test">
-    test
+<header class="bg-white">
+    <div class="container px-3 clearfix">
+        <a class="logo float-start" href="https://mgu-mlt.ru/"></a>
+        <div class="info float-end clearfix">
+            <div class="address float-start pt-3 ms-3 d-none d-md-block">
+                Адрес<br>
+                проспект Богдана Хмельницкого, 18
+            </div>
+            <div class="phone float-start pt-3 ms-3">
+                Телефон<br>
+                +7 990 146 9279
+            </div>
+            <div class="col float-start pt-3 ms-3 d-none d-lg-block  ">
+                Часы работы<br>
+                Пн - Пт с 8:00 до 16:30
+            </div>
+        </div>
+    </div>
+</header>
+<div class="content">
+    <div class="container">
+        <div class="my-2rem bg-light bg-white px-5 py-4 info-block-1 position-relative">
+            <h4 class="fs-24 fw-bolder m-0">
+                Давайте пройдем первичное анкетирование Вашей идеальной IT-профессии
+            </h4>
+            <div class="color-grey pt-3">
+                Так вы сможете предварительно узнать, какая IT-профессия может Вам подойти!
+            </div>
+        </div>
+    </div>
 </div>
-<main class="container-lg px-2">
-
-<?=$content??""?>
-</main>
 <footer>
 </footer>
 </body>
